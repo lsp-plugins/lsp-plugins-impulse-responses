adcf4d6e46859e94d55c23cf218099e17c4b0048
Vladimir Sadovnikov <sadko4u@gmail.com>
2021-08-17 00:54:58 +0300

    Updated dependencies

--- project.mk
+++ project.mk
@@ -21,7 +21,7 @@
 # Package version
 ARTIFACT_ID                 = LSP_PLUGINS_IMPULSE_RESPONSES
 ARTIFACT_NAME               = lsp-plugins-impulse-responses
-ARTIFACT_DESC               = Trigger plugin series
+ARTIFACT_DESC               = Impulse Responses plugin series
 ARTIFACT_HEADERS            = lsp-plug.in
 ARTIFACT_EXPORT_ALL         = 1
 ARTIFACT_VERSION            = 1.0.3-devel
