e2e8166c51e66ef4d7036ef43562bb451f91cca1
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-01-03 19:48:33 +0300

    Updated build scripts and dependencies

--- project.mk
+++ project.mk
@@ -24,6 +24,7 @@ ARTIFACT_NAME               = lsp-plugins-impulse-responses
 ARTIFACT_TYPE               = plug
 ARTIFACT_DESC               = LSP Impulse Responses Plugin Series
 ARTIFACT_HEADERS            = lsp-plug.in
+ARTIFACT_EXPORT_HEADERS     = 0
 ARTIFACT_VERSION            = 1.0.9-devel
 
 
