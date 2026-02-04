8d0b687c6d02bf32712f0250aaf86db419bd50e4
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-02-04 23:42:58 +0300

    Using new mechanism for registering plugin metadata

--- src/main/meta/impulse_responses.cpp
+++ src/main/meta/impulse_responses.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2025 Linux Studio Plugins Project <https://lsp-plug.in/>
- *           (C) 2025 Vladimir Sadovnikov <sadko4u@gmail.com>
+ * Copyright (C) 2026 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2026 Vladimir Sadovnikov <sadko4u@gmail.com>
  *
  * This file is part of lsp-plugins-impulse-responses
  * Created on: 3 авг. 2021 г.
@@ -20,6 +20,7 @@
  */
 
 #include <lsp-plug.in/plug-fw/meta/ports.h>
+#include <lsp-plug.in/plug-fw/meta/registry.h>
 #include <lsp-plug.in/shared/meta/developers.h>
 #include <lsp-plug.in/common/status.h>
 
@@ -209,6 +210,7 @@ namespace lsp
             mono_plugin_port_groups,
             &impulse_responses_bundle
         };
+        LSP_REGISTER_METADATA(impulse_responses_mono);
 
         const meta::plugin_t  impulse_responses_stereo =
         {
@@ -239,5 +241,7 @@ namespace lsp
             stereo_plugin_port_groups,
             &impulse_responses_bundle
         };
+        LSP_REGISTER_METADATA(impulse_responses_stereo);
+
     } // namespace meta
 } // namespace lsp
